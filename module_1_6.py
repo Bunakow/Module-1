0947fe5aa5f5c5377c9bccd606d754df886e050d
Bunakow <dmitrijbunakov001@gmail.com>
2024-11-21 21:49:00 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+completed_homework=12
+hours_spent=1.5
+Course="Python"
+time_for_one_task=hours_spent/completed_homework
+print('Курс:',Course,',','всего задач:',completed_homework,',','затрачено часов:',hours_spent,',','среднее время выполнения:',time_for_one_task,'часа.')
+
+
