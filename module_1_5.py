72bfd988ca69ad738ffd8f883b5303818efd5eb1
Bunakow <dmitrijbunakov001@gmail.com>
2024-11-26 22:31:23 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var =1, 2, "a", "b"
+print(immutable_var)
+#immutable_var[0]=3
+#Кортеж - не изменняемый тип данных
+mutable_list=[1,2,"a","b"]
+mutable_list.append("Modified")
+print(mutable_list)
