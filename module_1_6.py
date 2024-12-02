a7a9a022cf63ab3a57b7a223ae880dfda9e1d79e
Bunakow <dmitrijbunakov001@gmail.com>
2024-12-02 22:27:45 +0500

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,7 +1,20 @@
-completed_homework=12
-hours_spent=1.5
-Course="Python"
-time_for_one_task=hours_spent/completed_homework
-print('Курс:',Course,',','всего задач:',completed_homework,',','затрачено часов:',hours_spent,',','среднее время выполнения:',time_for_one_task,'часа.')
+my_dict = {'Sara':1985, 'Simon':1999, 'Valera':1977}
+print('Словарь:', my_dict)
+print('Год рождения Sara: ',my_dict['Sara'])
+print('Год рождения Dima:', my_dict.get('Dima', 'Нет такого ключа'))
+my_dict.update({'Diana':1998, 'Alex':2001})
+removed_year=my_dict.pop('Sara')
+print('Значение удалённого элемента \'Sara\':', removed_year)
+print('Изменённый словарь:', my_dict)
+my_set={1, 2, 3, 4, 5, 1, 2, 3, 4, True, 'Masha', "Masha"}
+print('Множество',my_set)
+my_set.add(11)
+my_set.add('Luna')
+my_set.remove(1)
+print('Изменённое множество',my_set)
+
+
+
+
 
 
